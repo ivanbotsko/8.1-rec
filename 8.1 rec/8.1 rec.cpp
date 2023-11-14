33be3dbd066cb5ebc4e4bbc3a7ab731523f18e8d
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-14 22:10:34 +0200

    first master commit

---
+++ 8.1 rec/8.1 rec.cpp
@@ -0,0 +1,61 @@
+﻿// Дано літерний рядок, який містить послідовність символів s[0], ..., s[n], ... 
+// 1. Визначити число входжень в послідовність групи букв “abc”.
+// 2. Замінити кожну групу букв “abc” парою зірочок “ * *”
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int Count(char* s, const char* cs)
+{
+    int k = 0,
+        pos = 0;
+    char* t;
+    while (t = strstr(s + pos, cs))
+    {
+        pos = t - s + 1;
+        k++;
+    }
+    return k;
+}
+
+char* Change(char* dest, const char* str, char* t, int i)
+{
+    if (str[i + 2] != 0)
+    {
+        if (str[i] == 'a' && str[i + 1] == 'b' && str[i + 2] == 'c')
+        {
+            strcat(t, "***");
+            return Change(dest, str, t + 3, i + 3);
+        }
+        else
+        {
+            *t++ = str[i++];
+            *t = '\0';
+            return Change(dest, str, t, i);
+        }
+    }
+    else
+    {
+        *t++ = str[i++];
+        *t++ = str[i++];
+        *t = '\0';
+        return dest;
+    }
+}
+
+int main()
+{
+    char s[101];
+    cout << "Enter string:" << endl;
+    cin.getline(s, 100);
+    cout << "String contained " << Count(s, "abc") << " groups of 'abc'" << endl;
+    char* dest1 = new char[151];
+    dest1[0] = '\0';
+    char* dest2;
+    dest2 = Change(dest1, s, dest1, 0);
+    cout << "Modified string (param) : " << dest1 << endl;
+    cout << "Modified string (result): " << dest2 << endl;
+    delete[] dest1; 
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "CppUnitTest.h"
+#include "../8.1 rec/8.1 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace YourProjectTests
+{
+    TEST_CLASS(YourTestClass)
+    {
+    public:
+
+        TEST_METHOD(TestCountFunction)
+        {
+            const char* testString1 = "abcabcabc";  // тестовий рядок з трьома групами "abc"
+            const char* testString2 = "ababab";     // тестовий рядок без груп "abc"
+            const char* testString3 = "";           // порожній рядок
+
+            const char* cs = "abc";
+
+            // Очікувані результати
+            int expectedCount1 = 3;
+            int expectedCount2 = 0;
+            int expectedCount3 = 0;
+
+            // Перевірка роботи функції Count
+            int result1 = Count(const_cast<char*>(testString1), cs); // const_cast is used to convert const char* to char*
+            int result2 = Count(const_cast<char*>(testString2), cs);
+            int result3 = Count(const_cast<char*>(testString3), cs);
+
+            // Перевірка результатів тестів
+            Assert::AreEqual(expectedCount1, result1, L"Test case 1 failed");
+            Assert::AreEqual(expectedCount2, result2, L"Test case 2 failed");
+            Assert::AreEqual(expectedCount3, result3, L"Test case 3 failed");
+        }
+    };
+}
